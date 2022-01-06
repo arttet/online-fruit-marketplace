9ed4fe4d8dd5b2411e45253b8a539a5197ed6c3a
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2022-01-06 20:38:26 +0300

    Add routes for deploy

--- website/src/routes.rs
+++ website/src/routes.rs
@@ -11,4 +11,20 @@ pub enum Route {
     #[not_found]
     #[at("/404")]
     NotFound,
+
+    // FIXME: workaround for deploy
+    #[at("/online-fruit-marketplace")]
+    SubRoute,
+}
+
+// FIXME: workaround for deploy
+#[derive(Routable, PartialEq, Clone, Debug)]
+pub enum SubRoute {
+    #[at("/online-fruit-marketplace/")]
+    Home,
+    #[at("/online-fruit-marketplace/product/:id")]
+    ProductDetail { id: i32 },
+    #[not_found]
+    #[at("/404")]
+    NotFound,
 }
